1db0ebd84852d4a41db504da8dbd0ab6efb8cc79
Arnaud Browet <abrowet@gmail.com>
2024-10-03 18:16:31 +0200

    udpate overflow behaviour

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="h-screen flex flex-col bg-prDarkBlue-25 pl-10 py-5 main-container">
+  <div class="h-dvh flex flex-col bg-prDarkBlue-25 pl-10 py-5 main-container">
     <main class="flex-1 overflow-auto flex flex-col pr-10">
       <slot></slot>
     </main>

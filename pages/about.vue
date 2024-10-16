0de623726b5b355e459a4048c437f38d96b368bc
PolarRES <ilya.pijpen@technopolis-group.com>
2024-10-16 10:20:08 +0200

    Update about description

--- pages/about.vue
+++ pages/about.vue
@@ -6,8 +6,7 @@
       <div class="prose bg-white max-w-2xl p-8">
         <h2 class="text-center">About PolarRes</h2>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque congue molestie lorem, id rhoncus ipsum. Ut sit amet est in urna maximus faucibus. Mauris posuere semper ligula, vitae
-          vestibulum mi aliquam vel. Nam at dictum lacus.
+          Despite the crucial role Polar Regions play in stabilising the global climate system, there are still critical knowledge gaps regarding the impact of polar climate processes on multiple scales and how they are evolving with our changing global climate. PolarRES studies the interactions between the atmosphere, oceans, and sea ice in the Arctic and Antarctic. By understanding these processes and interactions in conjunction with an innovative storylines approach we adopt in PolarRES, a refined set of climate projections can be provided for the Polar Regions. This paves way for a more reliable assessment of the potential social and environmental impacts of climate change.
         </p>
 
         <h2 class="text-center">The Storyboards</h2>
